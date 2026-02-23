910df657a23566dc50baac9e589cf2cf8d97442b
Eslamlinux <linuxeslam@gmail.com>
2026-02-23 22:18:14 +0200

    string in python

--- raytest/py17.py
+++ raytest/py17.py
@@ -1,3 +1,13 @@
 str = "one tow one four"
 
 print(str.replace(" one"," three",1))
+
+list = ["hi","hey","hello"]
+
+print(list)
+print(" ".join(list))
+print(" + ".join(list))
+print(" * ".join(list))
+print(" @ ".join(list))
+
+print(type(list))
