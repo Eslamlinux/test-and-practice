a7b5ca8fcfa8e51d2f8b4c9899348986b9b9ea8f
Eslamlinux <linuxeslam@gmail.com>
2026-02-24 22:43:33 +0200

    test c feture

---
+++ pythontest/testc.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+void main()
+{
+    char ss[] = "semsem";
+    printf("hello %s\n",ss);
+
+    float rank = 10.001;
+    printf("my ranke is : %.2f",rank);
+    printf("my ranke is : %.f",rank);
+
+}
