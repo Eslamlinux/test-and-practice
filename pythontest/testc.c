3a0a417b587ae5fd5f9bdd73a15337808c00076f
Eslamlinux <linuxeslam@gmail.com>
2026-02-24 23:41:10 +0200

    test c feture

--- pythontest/testc.c
+++ pythontest/testc.c
@@ -7,7 +7,7 @@ void main()
     printf("hello %s\n",ss);
 
     float rank = 10.001;
-    printf("my ranke is : %.2f",rank);
-    printf("my ranke is : %.f",rank);
-
+    printf("my ranke is : %.2f\n",rank);
+    printf("my ranke is : %f\n",rank);
+    printf("name %.3s\n",ss);
 }
