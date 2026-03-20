e120c5e8e3a81ee8aece2af493d29f08fe42bca6
Eslamlinux <linuxeslam@gmail.com>
2026-03-20 21:38:30 +0200

    list test

--- pythontest/py21.py
+++ pythontest/py21.py
@@ -1,4 +1,4 @@
-
+anotherlist = ["another"]
 thislist = ["apple", "banana", "cherry"]
 
 thislist[0] = "hello"
@@ -8,4 +8,13 @@ thislist.insert(0,"semsem")
 print(thislist)
 
 thislist[0:2] = ["zero","one"]
+print(thislist)
+
+thislist.append("hello")
+print(thislist)
+
+thislist.append(anotherlist)
+print(thislist)
+
+thislist.extend(anotherlist)
 print(thislist)
