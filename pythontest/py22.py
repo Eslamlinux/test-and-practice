abe98ed97696281bb5618323c30aa483ff275fa4
Eslamlinux <linuxeslam@gmail.com>
2026-03-23 21:45:29 +0200

    list test

--- pythontest/py22.py
+++ pythontest/py22.py
@@ -1,4 +1,4 @@
-fruits = ["apple", "banana", "cherry", "kiwi", "mango","bc"]
+fruits = ["apple", "banAna", "Cherry", "kiwi", "mango","bc"]
 newdta = []
 for n in fruits:
     if("c") in n:
@@ -8,5 +8,10 @@ print(newdta)
 
 
 newdta = [n for n in fruits if("a") in n]
+print("\n")
+print(newdta)
+
+
+newdta = [n.upper() for n in fruits if(n.lower()) in n]
 print("\n")
 print(newdta)
