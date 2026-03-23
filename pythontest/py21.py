99960b02ede29d1fd6687db391e826f9d2ec1c53
Eslamlinux <linuxeslam@gmail.com>
2026-03-23 21:16:39 +0200

    list test

--- pythontest/py21.py
+++ pythontest/py21.py
@@ -1,29 +1,32 @@
 anotherlist = ["another"]
 thislist = ["apple", "banana", "cherry"]
 
-thislist[0] = "hello"
-print(thislist)
+# thislist[0] = "hello"
+# print(thislist)
 
-thislist.insert(0,"semsem")
-print(thislist)
+# thislist.insert(0,"semsem")
+# print(thislist)
 
-thislist[0:2] = ["zero","one"]
-print(thislist)
+# thislist[0:2] = ["zero","one"]
+# print(thislist)
 
-thislist.append("hello")
-print(thislist)
+# thislist.append("hello")
+# print(thislist)
 
-thislist.append(anotherlist)
-print(thislist)
+# thislist.append(anotherlist)
+# print(thislist)
 
-thislist.extend(anotherlist)
-print(thislist)
+# thislist.extend(anotherlist)
+# print(thislist)
 
-thislist.clear()
-print(thislist)
+# thislist.clear()
+# print(thislist)
 
-print("%s %s"%("hello","semsem"))
-print("{} {}".format("hello","semsem"))
-nn = "hello"
-ns = "semsem"
-print(f"{"new method = "}{nn} {ns}")
+# print("%s %s"%("hello","semsem"))
+# print("{} {}".format("hello","semsem"))
+# nn = "hello"
+# ns = "semsem"
+# print(f"{"new method = "}{nn} {ns}")
+
+for n in thislist:
+    print(n)
