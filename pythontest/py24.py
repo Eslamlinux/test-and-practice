2dca95083abf67c15d069387800f113765192b32
Eslamlinux <linuxeslam@gmail.com>
2026-03-29 00:00:58 +0200

    tuple test

--- pythontest/py24.py
+++ pythontest/py24.py
@@ -8,6 +8,8 @@ print(x,y,z)
 mysetone = {"eslam","33"}
 # mysetone.clear()
 # mysetone2 = {"es",3,[1,2,3]}
+mysetone2 = {"es",3,(1,2,3)}
 print(mysetone)
 print(type(mysetone))
 # print(mysetone[0:])
+print(mysetone2)
