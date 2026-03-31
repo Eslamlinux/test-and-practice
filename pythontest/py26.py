f1dc8b2c94a2d110df458985e9d28898c1c3ccc2
Eslamlinux <linuxeslam@gmail.com>
2026-04-01 00:13:27 +0200

    set test

--- pythontest/py26.py
+++ pythontest/py26.py
@@ -1,6 +1,6 @@
 sett1 = {"hi","hello"}
 sett2 = {"hi","eslam"}
-print(type(sett1))
+# print(type(sett1))
 
 print(sett1.difference(sett2))
 print(sett2.difference(sett1))
@@ -8,7 +8,7 @@ print(sett2.difference(sett1))
 print(sett1.intersection(sett2))
 print(sett2.intersection(sett1))
 
-sett3 = {"helllo","cool"}
+sett3 = {"hello","cool","hi"}
 sett1.intersection_update(sett3)
 print(sett1)
 
