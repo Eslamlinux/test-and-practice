ecab3b08bce07ffe287784f0291d89865d36c9d3
Eslamlinux <linuxeslam@gmail.com>
2026-04-04 21:57:21 +0200

    tuple test

---
+++ pythontest/py31.py
@@ -0,0 +1,10 @@
+def startwork():
+    entry = input("please Enter the value: ")
+    if entry == "x":
+        return
+    setdata = {entry}
+    if entry == "p":
+        print(entry)
+
+while(True):
+    startwork()
