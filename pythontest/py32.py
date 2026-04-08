d447a7e256e2f1240adb4bf468e524e4ebd74a65
Eslamlinux <linuxeslam@gmail.com>
2026-04-08 07:36:45 +0200

    dic test

---
+++ pythontest/py32.py
@@ -0,0 +1,20 @@
+dic = {
+    0:"eslam",
+    1:"khalid",
+    2:"hassan"
+}
+
+print(dic.get(0))
+print(dic.keys())
+print(dic.values())
+print(dic.items())
+
+d_tup_list = dic.items()
+print(d_tup_list)
+dic[0] = "hassan"
+print(d_tup_list)
+
+print(dic.get(0))
+dic.update({0:"eslam"})
+print(dic.get(0))
+
