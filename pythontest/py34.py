b2f9c20cc4e0420d11747ebf777b3a7f10bdcf90
Eslamlinux <linuxeslam@gmail.com>
2026-04-10 14:43:06 +0200

    dic test

--- pythontest/py34.py
+++ pythontest/py34.py
@@ -1,4 +1,12 @@
-dic1 = {1,2,3}
+dic1 = {1:"one",2:"tow",3:"three"}
+# canot copy like that it will be refrance only
 dic2 = dic1
 
+dic1[4] = 4
 print(dic2)
+
+# right way to copy item
+dic3 = dic1.copy()
+print(dic3)
+dic1[5] = "hi"
+print(dic3)
