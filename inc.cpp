87a99bb2bd5d62c75747a20d05b3c4f326699168
Eslamlinux <linuxeslam@gmail.com>
2026-04-11 00:55:22 +0200

    dic test

--- inc.cpp
+++ inc.cpp
@@ -7,13 +7,16 @@ int main()
     int a = 10;
     if(a > 1)
     {
-        std::cout << "a > 1";
+        std::cout << "a > 1\n";
     }
     else if(a > 2)
     {
-        std::cout << "a > 2";
+        std::cout << "a > 2\n";
     }
-
+    else if(a > 3)
+    {
+        std::cout << "a > 3\n";
+    }
     
     return 0;
 }
