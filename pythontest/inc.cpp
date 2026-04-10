9a33040331d7a366726a4c8cb9316da5b869c51f
Eslamlinux <linuxeslam@gmail.com>
2026-04-11 00:50:20 +0200

    dic test

---
+++ pythontest/inc.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+
+int main()
+{
+    int a = 10;
+    if(a > 1)
+    {
+        std::cout << "a > 1";
+    }
+    else if(a > 2)
+    {
+        std::cout << "a > 2";
+    }
+
+    
+    return 0;
+}
