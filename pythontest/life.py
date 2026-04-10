9d7bed7bafff6232fc5c27067e79bed2215aabe2
Eslamlinux <linuxeslam@gmail.com>
2026-04-10 22:46:25 +0200

    dic test

--- pythontest/life.py
+++ pythontest/life.py
@@ -2,4 +2,9 @@
 Age = int(input("Enter youre Age: "))
 
 days = Age * 365
-print(f"days is {days:,}")
+if Age < 100 and Age > 1:
+ print(f"firs your days is {days:,}")
+elif Age >= 100 and Age < 130:
+ print(f"cool days is {days:,}")
+else:
+ print("not included")
