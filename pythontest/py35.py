9eab8cf1c009d1fe24e6a5630ee8f83724c349df
Eslamlinux <linuxeslam@gmail.com>
2026-04-10 22:14:07 +0200

    dic test

--- pythontest/py35.py
+++ pythontest/py35.py
@@ -1,11 +1,14 @@
 
 fname = input("What's your name? ")
 lname = input("what's your father name? ")
-yemail = input("enter your email\n")
+yemail = input("enter your email\n").strip()
 fname = fname.strip().capitalize()
 lname = lname.strip().capitalize()
 usern = yemail[:yemail.index("@")]
 website = yemail[yemail.index("@") + 1:]
+
+usern = usern.strip()
+website = website.strip()
 print(f"Hello {fname} {lname} How Are You!")
 print(f"your-user name is: {usern} from websit name {website}")
   
