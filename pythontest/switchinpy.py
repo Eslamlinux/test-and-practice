5221958716202eb7d36c075a74b0c92ea6b7a558
Eslamlinux <linuxeslam@gmail.com>
2026-04-12 01:57:05 +0200

    test if in cpp

---
+++ pythontest/switchinpy.py
@@ -0,0 +1,15 @@
+
+
+switch = 4
+
+match switch:
+    case 1:
+        print(1)
+    case 2:
+        print(2)
+    case 3:
+        print(3)
+    case 4:
+        print(4)
+    case _:
+        print("default")
