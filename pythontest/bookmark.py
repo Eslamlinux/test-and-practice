1067f844ad14e5caacf1195bfae96ce531345d5e
Eslamlinux <linuxeslam@gmail.com>
2026-04-12 01:24:41 +0200

    test if in cpp

--- pythontest/bookmark.py
+++ pythontest/bookmark.py
@@ -1,6 +1,11 @@
 data = []
 
-web = input("enter the url without https://www.   or .com\n")
-data.append(f"https://www.{web}.com")
+much = 5
+while much > 0:
+    web = input("enter the url without https://www.   or .com\n")
+    data.append(f"https://www.{web}.com")
+    much -=1
+    print(f"{len(data)} added , {much} left")
 
-print(data)
+for x in data:
+    print(x)
