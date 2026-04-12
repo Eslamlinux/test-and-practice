31fab03737d070b4f94b54bf5c68b7e7fb311781
Eslamlinux <linuxeslam@gmail.com>
2026-04-12 13:50:12 +0200

    test if in cpp

--- pythontest/switchinpy.py
+++ pythontest/switchinpy.py
@@ -1,7 +1,7 @@
 
 
 switch = 4
-
+a = 10
 match switch:
     case 1:
         print(1)
@@ -9,9 +9,16 @@ match switch:
         print(2)
     case 3:
         print(3)
-    case 4:
+    case 4 if a > 5:
         print(4)
     case 5|6|7|8|9:
         print("from 5 to 9")
     case _:
         print("default")
+
+i = 10
+while i > 0 :
+    i -=1
+    if i == 5:
+        continue
+    print(i)
