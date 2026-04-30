387141edd0a2c982429421d9a563359bd6a9c54d
Eslam Linux <linuxeslam@gmail.com>
2026-04-30 22:11:34 +0300

    Update DetectPangram.cpp

--- DetectPangram.cpp
+++ DetectPangram.cpp
@@ -23,6 +23,7 @@ https://www.codewars.com/kata/54dc6f5a224c26032800005c/train/cpp
 "ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"
 
 */
+//
 #include <iostream>
 #include <vector>
 #include <string>
