8d9e8b5238c6af0592ad749b520f003790db1975
Eslam Linux <linuxeslam@gmail.com>
2026-06-07 22:42:28 +0300

    Update Doyouspeaken.cpp

--- Doyouspeaken.cpp
+++ Doyouspeaken.cpp
@@ -6,7 +6,7 @@ The order of characters is important -- a string "abcEnglishdef" is correct but 
 Upper or lower case letter does not matter -- "eNglisH" is also correct.
 
 Return value as boolean values, true for the string to contains "English", false for it does not.
-
+888
 */
 
 //
